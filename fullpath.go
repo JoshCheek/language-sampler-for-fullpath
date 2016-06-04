f2bf2c94d18fde3aeb6d004e204f05b989e8138d
Josh Cheek <josh.cheek@gmail.com>
2016-06-04 00:42:26 -0600

    Layout the structure

--- fullpath.go
+++ fullpath.go
@@ -3,6 +3,40 @@ package main
 import "os"
 import "fmt"
 
+func doHelp(args []string) bool {
+	return false // FIXME
+}
+
+func doCopy(args []string) bool {
+	return false // FIXME
+}
+
+func mapToFullPaths(relativePaths []string) []string {
+	return relativePaths // FIXME
+}
+
+func selectPaths(paths []string) []string {
+	return paths // FIXME
+}
+
+func join(fullpaths []string, delimiter string) string {
+	return delimiter // FIXME
+}
+
+func copyToClipboard(str string) {
+	// FIXME
+}
+
 func main() {
-	fmt.Println(os.Args)
+	args := os.Args[1:]
+	if doHelp(args) {
+		fmt.Println("this is the help screen")
+	} else {
+		fullpaths := mapToFullPaths(selectPaths(args))
+		pathsString := join(fullpaths, "\n")
+		fmt.Println(pathsString)
+		if doCopy(args) {
+			copyToClipboard(pathsString)
+		}
+	}
 }
