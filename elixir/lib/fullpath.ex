dd383306e595f503591f3a11684220bb2394d55d
Josh Cheek <josh.cheek@gmail.com>
2016-06-14 21:28:27 -0600

    Elixir solution: Does not print a trailing newline when there is a single path

--- elixir/lib/fullpath.ex
+++ elixir/lib/fullpath.ex
@@ -1,8 +1,8 @@
 defmodule Fullpath do
   def main(args) do
-    args = remove_empty(args)
-    args = expand_args(args, System.cwd())
-    Enum.each args, &IO.puts(&1)
+    paths = remove_empty(args)
+    paths = expand_args(paths, System.cwd())
+    print_paths paths
   end
 
   def remove_empty(args) do
@@ -15,6 +15,16 @@ defmodule Fullpath do
 
   def expand(relative_path, working_dir) do
     Path.expand relative_path, working_dir
+  end
+
+  def print_paths(paths) do
+    toPrint = Enum.join paths, "\n"
+    if 1 < length(paths) do
+      toPrint = toPrint <> "\n"
+    end
+    IO.write(toPrint)
+    # paths = Enum.map paths, fn(path) -> path + "\n" end
+    # Enum.each args, &IO.puts(&1)
   end
 end
 
