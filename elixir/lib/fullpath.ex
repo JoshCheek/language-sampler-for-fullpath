f06064acc2526fe593bb61b12d29a865d0197705
Josh Cheek <josh.cheek@gmail.com>
2016-06-14 21:08:45 -0600

    Elixir solution: Simple path printing

--- elixir/lib/fullpath.ex
+++ elixir/lib/fullpath.ex
@@ -1,6 +1,26 @@
 defmodule Fullpath do
   def main(args) do
-    IO.puts("Hello, world")
-    IO.puts(args)
+    args = remove_empty(args)
+    args = expand_args(args, System.cwd())
+    Enum.each args, &IO.puts(&1)
+  end
+
+  def remove_empty(args) do
+    Enum.filter(args, fn(arg) -> arg != "" end)
+  end
+
+  def expand_args(args, working_dir) do
+    Enum.map(args, fn(arg) -> expand(arg, working_dir) end)
+  end
+
+  def expand(relative_path, working_dir) do
+    Path.expand(relative_path, working_dir)
   end
 end
+
+Fullpath.main(["abc", "", "ghi"])
+
+# Maybe useful?
+# IO.inspect(args)
+# List.first(args)
+# IO.puts("Hello, world")
