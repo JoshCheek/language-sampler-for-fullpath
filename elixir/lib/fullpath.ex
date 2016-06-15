1134d2909cdfe9dd39ee5f247b2d3b66614eb0b0
Josh Cheek <josh.cheek@gmail.com>
2016-06-15 16:34:41 -0600

    Elixir solution: Remove some unnecessary methods, refactor syntax

--- elixir/lib/fullpath.ex
+++ elixir/lib/fullpath.ex
@@ -24,33 +24,27 @@ defmodule Fullpath do
       The -c flag will copy the results into your pasteboard
     """
 
-  def includes?(haystack, needle),        do: find(haystack, &needle==&1)
-  def expand(relative_path, working_dir), do: Path.expand relative_path, working_dir
-  def flag?(string),                      do: String.starts_with? string, "-"
-  def chomp(string),                      do: String.rstrip(string, ?\n)
-  def empty_string?(string),              do: 0 == String.length(string)
-  def empty_list?(list),                  do: 0 == length list
-  def to_paths(potential_paths),          do: potential_paths
-                                              |> map(&chomp/1)
-                                              |> filter(&!empty_string?(&1))
-                                              |> filter(&!flag?(&1))
+  def includes?(haystack, needle),      do: find haystack, &needle==&1
+  def flag?(string),                    do: String.starts_with? string, "-"
+  def empty_string?(string),            do: 0 == String.length(string)
+  def empty_list?(list),                do: 0 == length list
+  def expand_paths(paths, working_dir), do: map paths, &Path.expand(&1, working_dir)
+  def to_paths(potential_paths),        do: potential_paths
+                                            |> filter(&!empty_string? &1)
+                                            |> filter(&!flag? &1)
 
   def get_paths(args, io, cwd) do
     paths = to_paths args
-    if empty_list?(paths), do: paths = to_paths io.stream(:stdio, :line)
-    expand_args paths, cwd
+    if empty_list? paths do
+      paths = to_paths io.stream(:stdio, :line)
+    end
+    expand_paths paths, cwd
   end
 
   def copy_to_pasteboard(string) do
     port = Port.open {:spawn, "pbcopy"}, [:stream, :binary, :exit_status, :use_stdio]
     Port.command port, string
     Port.close port
-  end
-
-
-  def expand_args(args, working_dir) do
-    expand_in_working_dir = fn(arg) -> expand(arg, working_dir) end
-    map args, expand_in_working_dir
   end
 
   def format(paths) do
