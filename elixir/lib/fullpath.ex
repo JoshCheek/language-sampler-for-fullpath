4cf1db138c6ee7e07990a86ec8d4e9be7cca42b6
Josh Cheek <josh.cheek@gmail.com>
2016-06-15 16:24:40 -0600

    Elixir solution: Terser syntax

--- elixir/lib/fullpath.ex
+++ elixir/lib/fullpath.ex
@@ -11,16 +11,9 @@ defmodule Fullpath do
     end
   end
 
-  def help?(args) do
-    includes?(args, "-h") || includes?(args, "--help")
-  end
-
-  def copy?(args) do
-    includes?(args, "-c") || includes?(args, "--copy")
-  end
-
-  def help_screen do
-    """
+  def help?(args), do: includes?(args, "-h") || includes?(args, "--help")
+  def copy?(args), do: includes?(args, "-c") || includes?(args, "--copy")
+  def help_screen, do: """
     usage: fullpath *[relative-paths] [-c]
 
       Prints the fullpath of the paths
@@ -30,36 +23,34 @@ defmodule Fullpath do
 
       The -c flag will copy the results into your pasteboard
     """
-  end
+
+  def includes?(haystack, needle),        do: find(haystack, &needle==&1)
+  def expand(relative_path, working_dir), do: Path.expand relative_path, working_dir
+  def flag?(string),                      do: String.starts_with? string, "-"
+  def chomp(string),                      do: String.rstrip(string, ?\n)
+  def empty_string?(string),              do: 0 == String.length(string)
+  def empty_list?(list),                  do: 0 == length list
+  def to_paths(potential_paths),          do: potential_paths
+                                              |> map(&chomp/1)
+                                              |> filter(&!empty_string?(&1))
+                                              |> filter(&!flag?(&1))
 
   def get_paths(args, io, cwd) do
     paths = to_paths args
-    if empty_list? paths do
-      paths = to_paths io.stream(:stdio, :line)
-    end
-    paths |> expand_args(cwd)
+    if empty_list?(paths), do: paths = to_paths io.stream(:stdio, :line)
+    expand_args paths, cwd
   end
 
   def copy_to_pasteboard(string) do
-    port = Port.open({:spawn, "pbcopy"}, [:stream, :binary, :exit_status, :use_stdio])
+    port = Port.open {:spawn, "pbcopy"}, [:stream, :binary, :exit_status, :use_stdio]
     Port.command port, string
     Port.close port
   end
 
-  def includes?(haystack, needle) do
-    find(haystack, &needle==&1)
-  end
-
-  def to_paths(potential_paths) do
-    potential_paths |> map(&chomp/1) |> filter(&!empty_string?(&1)) |> filter(&!flag?(&1))
-  end
 
   def expand_args(args, working_dir) do
-    map args, fn(arg) -> expand(arg, working_dir) end
-  end
-
-  def expand(relative_path, working_dir) do
-    Path.expand relative_path, working_dir
+    expand_in_working_dir = fn(arg) -> expand(arg, working_dir) end
+    map args, expand_in_working_dir
   end
 
   def format(paths) do
@@ -68,21 +59,5 @@ defmodule Fullpath do
       toPrint = toPrint <> "\n"
     end
     toPrint
-  end
-
-  def flag?(string) do
-    String.starts_with? string, "-"
-  end
-
-  def chomp(string) do
-    String.rstrip(string, ?\n)
-  end
-
-  def empty_string?(string) do
-    0 == String.length(string)
-  end
-
-  def empty_list?(list) do
-    0 == length list
   end
 end
