0bdc0bdb5e44f03bb3fa4eaf89688b0557a1a82e
Josh Cheek <josh.cheek@gmail.com>
2016-06-28 16:03:44 -0500

    Uhm, go away

--- common-lisp/notes.lisp
+++ common-lisp/notes.lisp
@@ -5,11 +5,21 @@
   ; Basic docs
   (describe 'function-name)
 
+  ; list the SYMBOLs matching name.
+  (apropos name)
+
+  ; There's a bunch that have the word "stream" in them, might work here
+  *standard-input*
+
+  ; This thing is a normal list
+  *args*
+
+  ; Fuck this thing
+  (argv)
 
 ; Useful
   (setq myvar someval)
   (eq 1 1) ; => T
   (eq 1 2) ; => NIL
   (not T)  ; => NIL
-  (make-pathname :name "abc" :directory '(:absolute "lol" "wat"))
-  ; => #P"/lol/wat/abc"
+  (make-pathname :name "abc" :directory '(:absolute "lol" "wat")) ; => #P"/lol/wat/abc"
