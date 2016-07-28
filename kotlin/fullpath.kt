1585138bcbaa5c157368782d038b6d977bf8c083
Josh Cheek <josh.cheek@gmail.com>
2016-07-28 15:45:50 -0500

    Kotlin: Prints the help screen, refactor to use invocation data structure

--- kotlin/fullpath.kt
+++ kotlin/fullpath.kt
@@ -4,13 +4,45 @@ import java.io.InputStreamReader
 import java.lang.System
 import java.io.PrintStream
 
-val dir    = System.getProperty("user.dir")
-var stdin  = BufferedReader(InputStreamReader(System.`in`))
-var stdout = System.`out`
+data class Invocation(
+  val args       : Array<String>,
+  val paths      : List<String>,
+  val dir        : String,
+  val reader     : BufferedReader,
+  val writer     : PrintStream,
+  val printHelp  : Boolean,
+  val copyResult : Boolean,
+  val helpScreen : String = """
+  usage: fullpath *[relative-paths] [-c]
+
+    Prints the fullpath of the paths
+    If no paths are given as args, it will read them from stdin
+
+    If there is only one path, the trailing newline is omitted
+
+    The -c flag will copy the results into your pasteboard
+  """.trimIndent()
+)
 
 fun main(args : Array<String>) {
-  val paths = getPaths(stdin, args.asList(), dir)
-  printPaths(stdout, paths)
+  invoke(withPaths(Invocation(
+    args       = args,
+    paths      = emptyList<String>(),
+    dir        = System.getProperty("user.dir"),
+    reader     = BufferedReader(InputStreamReader(System.`in`)),
+    writer     = System.`out`,
+    printHelp  = args.contains("-h") || args.contains("--help"),
+    copyResult = args.contains("-c") || args.contains("--copy")
+  )))
+}
+
+fun invoke(invocation:Invocation) {
+  if (invocation.printHelp) {
+    println(invocation.helpScreen)
+  } else {
+    // copy maybe
+    printPaths(invocation.writer, invocation.paths)
+  }
 }
 
 fun printPaths(outStream:PrintStream, paths:List<String>) =
@@ -19,12 +51,24 @@ fun printPaths(outStream:PrintStream, paths:List<String>) =
   else
     paths.forEach { outStream.println(it) }
 
-fun getPaths(inStream:BufferedReader, args:List<String>, dir:String):List<String> {
-  var lines = filterBlanks(breakNewlines(args))
-  if (lines.isEmpty())
-    lines = filterBlanks(readLines(inStream))
-  return lines.map { "$dir/$it" }
+fun withPaths(invocation:Invocation):Invocation {
+  var paths:List<String>
+  paths = invocation.args.asList()
+  paths = filterBlanks(paths)
+  paths = filterFlags(paths)
+  paths = breakNewlines(paths)
+  paths = filterBlanks(paths)
+  if (paths.isEmpty()) {
+    paths = readLines(invocation.reader)
+    paths = filterBlanks(paths)
+  }
+  val dir = invocation.dir
+  return invocation.copy(paths=paths.map { "$dir/$it" })
 }
+
+val dash = "-"[0]
+fun filterFlags(args:List<String>):List<String> =
+  args.filter { it[0] != dash }
 
 fun filterBlanks(strings:List<String>):List<String> =
   strings.filter { it != "" }
