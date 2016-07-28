a349ae65c7db98b469b1901feb7145b0185738b1
Josh Cheek <josh.cheek@gmail.com>
2016-07-28 13:27:24 -0500

    Kotlin: Refactor, push deps to main, behaviour out of main

--- kotlin/fullpath.kt
+++ kotlin/fullpath.kt
@@ -2,24 +2,29 @@ import kotlin.CharSequence
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.lang.System
+import java.io.PrintStream
 
 fun main(args : Array<String>) {
-  val dir   = System.getProperty("user.dir")
-  var stdin = BufferedReader(InputStreamReader(System.`in`));
-  val paths = getPaths(stdin, args.asList())
-
-  if (paths.size == 1)
-    print("${dir}/${paths[0]}")
-  else
-    for (path in paths)
-      println("${dir}/${path}")
+  val dir    = System.getProperty("user.dir")
+  var stdin  = BufferedReader(InputStreamReader(System.`in`))
+  var stdout = System.`out`
+  val paths  = getPaths(stdin, args.asList(), dir)
+  printPaths(stdout, paths)
 }
 
-fun getPaths(inStream:BufferedReader, args:List<String>):List<String> {
-  val lines = filterBlanks(breakNewlines(args))
+fun printPaths(outStream:PrintStream, paths:List<String>) {
+  if (paths.size == 1)
+    outStream.print(paths[0])
+  else
+    for (path in paths)
+      outStream.println(path)
+}
+
+fun getPaths(inStream:BufferedReader, args:List<String>, dir:String):List<String> {
+  var lines = filterBlanks(breakNewlines(args))
   if (lines.isEmpty())
-    return filterBlanks(readLines(inStream))
-  return lines
+    lines = filterBlanks(readLines(inStream))
+  return lines.map { line -> "${dir}/${line}" }
 }
 
 fun filterBlanks(strings:List<String>):List<String> {
