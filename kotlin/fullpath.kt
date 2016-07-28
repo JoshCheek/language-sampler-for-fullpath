59f6da0c9b458d87746d9572ec604f32dd802951
Josh Cheek <josh.cheek@gmail.com>
2016-07-28 13:12:21 -0500

    Kotlin: Pass stdin and syntactic refactorings
    Better to pass the dependency than have it be implicit
    Figured out how to do it from this Java code (took a while to put this
    all together):
    
    ```
    import java.io.BufferedReader;
    import java.io.InputStreamReader;
    
    public class Hello {
      public static void main(String[] args) {
        BufferedReader inStream = new BufferedReader(new InputStreamReader(System.in));
        String line = null;
        try {
          line = inStream.readLine();
        } catch(java.io.IOException e) {
          System.out.println("No lines available!");
          return;
        }
        System.out.println("Your line: " + line);
      }
    }
    ```
    
    And finally figured out to use backticks on the `in` from this post:
    https://discuss.kotlinlang.org/t/input/1078/3

--- kotlin/fullpath.kt
+++ kotlin/fullpath.kt
@@ -1,48 +1,43 @@
 import kotlin.CharSequence
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.System
 
 fun main(args : Array<String>) {
-  val dir = System.getProperty("user.dir")
+  val dir   = System.getProperty("user.dir")
+  var stdin = BufferedReader(InputStreamReader(System.`in`));
+  val paths = getPaths(stdin, args.asList())
 
-  val paths = getPaths(args.asList())
-  if (paths.size == 1) {
+  if (paths.size == 1)
     print("${dir}/${paths[0]}")
-    return
-  }
-  for (word in paths) {
-    println("${dir}/${word}")
-  }
+  else
+    for (path in paths)
+      println("${dir}/${path}")
 }
 
-fun getPaths(args : List<String>):List<String> {
+fun getPaths(inStream:BufferedReader, args:List<String>):List<String> {
   val lines = filterBlanks(breakNewlines(args))
-  if (lines.isEmpty()) {
-    return filterBlanks(readLines())
-  }
+  if (lines.isEmpty())
+    return filterBlanks(readLines(inStream))
   return lines
 }
 
-fun filterBlanks(strings : List<String>):List<String> {
+fun filterBlanks(strings:List<String>):List<String> {
   return strings.filter { a -> a != "" }
 }
 
-fun breakNewlines(rawPaths : List<String>):List<String> {
+fun breakNewlines(rawPaths:List<String>):List<String> {
   return rawPaths.flatMap { str -> str.split("\n") }
 }
 
-fun readLines():List<String> {
+fun readLines(inStream:BufferedReader):List<String> {
   var newArgs = mutableListOf<String>()
   while (true) {
-    val line = readLine()
-    if(line != null) {
+    val line = inStream.readLine()
+    if(line != null)
       newArgs.add(line)
-    } else {
+    else
       break
-    }
   }
   return newArgs
 }
-
-/* Maybe useful:
-
-  val paths = arrayOf<String>()
-*/
