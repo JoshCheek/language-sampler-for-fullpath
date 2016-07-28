cc79bad4ecb279da8ea41668a75fb2610c2c021c
Josh Cheek <josh.cheek@gmail.com>
2016-07-28 12:26:22 -0500

    Kotlin: Compiles again, uses lists instead of arrays

--- kotlin/fullpath.kt
+++ kotlin/fullpath.kt
@@ -1,18 +1,50 @@
-import kotlin.io.readLine
+import kotlin.CharSequence
 
 fun main(args : Array<String>) {
-  val path = System.getProperty("user.dir")
+  val dir = System.getProperty("user.dir")
 
-  if (args.size == 0) {
-    print("${path}/${readLine()}")
+  val paths = getPaths(args.asList())
+  if (paths.size == 1) {
+    print("${dir}/${paths[0]}")
     return
   }
-
-  if (args.size == 1) {
-    print("${path}/${args[0]}")
-    return
+  for (word in paths) {
+    println("${dir}/${word}")
   }
-
-  for (arg in args)
-    if (arg != "\n") println("${path}/${arg}")
 }
+
+fun getPaths(args : List<String>):List<String> {
+  val lines = filterBlanks(breakNewlines(args))
+  /* if (lines.length == 0) { */
+  /*   return filterBlanks(readLines()) */
+  /* } */
+  return lines
+}
+
+fun filterBlanks(strings : List<String>):List<String> {
+  return strings
+/*   return strings.filter { a -> a != "" } */
+}
+
+fun breakNewlines(rawPaths : List<String>):List<String> {
+  return rawPaths.flatMap { str -> str.split("\n") }
+  /* val paths = arrayOf<String>() */
+  /* for (rawPath in rawPaths) { */
+  /*   for (line in rawPath.split("\n")) { */
+  /*     paths.push(line) */
+  /*   } */
+  /* } */
+  /* return paths */
+}
+
+/* fun readLines() { */
+/*   var newArgs = [] */
+/*   while (true) { */
+/*     val line = readLine() */
+/*     if(!line) { */
+/*       break */
+/*     } */
+/*     newArgs.push(line) */
+/*   } */
+/*   return newArgs */
+/* } */
