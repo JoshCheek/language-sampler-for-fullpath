7c2109f5cbfea5145affe7c8b99c8c2aa4d0e9ed
Josh Cheek <josh.cheek@gmail.com>
2016-07-30 06:23:29 -0500

    C: Prints CWD with the path

--- c/fullpath.c
+++ c/fullpath.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <unistd.h>
 
-void print_paths(char const *paths[], int num_paths) {
+void output(char const *paths[], int num_paths, char *dir) {
   int i;
   if(num_paths == 1)
-    printf("%s", paths[0]);
+    printf("%s/%s", dir, paths[0]);
   else
     for(i=0; i<num_paths; ++i)
-      printf("%s\n", paths[i]);
+      printf("%s/%s\n", dir, paths[i]);
 }
 
 int main(int argc, char const *argv[]) {
-  print_paths(argv+1, argc-1);
+  char cwd[1024] = "";
+  getcwd(cwd, sizeof(cwd));
+  output(argv+1, argc-1, cwd);
   return 0;
 }
