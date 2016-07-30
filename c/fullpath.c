88862d99841272925d3981d5619a455d4de61043
Josh Cheek <josh.cheek@gmail.com>
2016-07-30 07:22:58 -0500

    C: Print the help screen

--- c/fullpath.c
+++ c/fullpath.c
@@ -30,6 +30,17 @@ int remove_string(char **strings, int*num_strings, char *to_remove) {
   return removed;
 }
 
+void print_help() {
+  printf("usage: fullpath *[relative-paths] [-c]\n");
+  printf("\n");
+  printf("  Prints the fullpath of the paths\n");
+  printf("  If no paths are given as args, it will read them from stdin\n");
+  printf("\n");
+  printf("  If there is only one path, the trailing newline is omitted\n");
+  printf("\n");
+  printf("  The -c flag will copy the results into your pasteboard\n");
+}
+
 int main(int argc, char **argv) {
   // get current working directory
   char cwd[1024];
@@ -41,6 +52,11 @@ int main(int argc, char **argv) {
   remove_empties(paths, &num_paths);
   int do_help   = remove_string(paths, &num_paths, "-h") | remove_string(paths, &num_paths, "--help");
   int do_copy   = remove_string(paths, &num_paths, "-c") | remove_string(paths, &num_paths, "--copy");
+
+  if(do_help) {
+    print_help();
+    return 0;
+  }
 
   // output
   output(paths, num_paths, cwd);
