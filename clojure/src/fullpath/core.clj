3f843aa2c8464ea2287f6002800766d3c99e3788
Josh Cheek <josh.cheek@gmail.com>
2016-07-30 23:38:15 -0500

    Clojure: Prints the paths in the expected format

--- clojure/src/fullpath/core.clj
+++ clojure/src/fullpath/core.clj
@@ -3,8 +3,14 @@
 (defn filter-blank [args]
   (filter #(not (clojure.string/blank? %)) args))
 
+
+(defn format-paths [paths]
+  (let [chomp          #(clojure.string/trim-newline %)
+        normalize-path #(str (chomp %) "\n")]
+  (clojure.string/join (map normalize-path paths))))
+
 (defn -main [& argv]
   (let [cwd       (System/getProperty "user.dir")
         paths     (filter-blank argv)
         fullpaths (map #(str cwd "/" %) paths)]
-    (println fullpaths)))
+    (print (format-paths fullpaths))))
