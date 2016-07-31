829ee52178df4e1c1af0f70ac5224d6a6760e39a
Josh Cheek <josh.cheek@gmail.com>
2016-07-31 00:55:19 -0500

    Clojure: paths from stdin get pathified and proper newlining

--- clojure/src/fullpath/core.clj
+++ clojure/src/fullpath/core.clj
@@ -5,16 +5,21 @@
   (filter #(not (clojure.string/blank? %)) args))
 
 
-(defn format-paths [paths]
+(defn format-paths [cwd paths]
   (let [chomp             #(clojure.string/trim-newline %)
-        ends-with-newline #(str (chomp %) "\n")]
+        ends-with-newline #(str (chomp %) "\n")
+        pathify           #(str cwd "/" %)
+        ]
     (if (= 1 (count paths))
-        (chomp (first paths))
-        (clojure.string/join (map ends-with-newline paths)))))
+        (pathify (chomp (first paths)))
+        (clojure.string/join (map #(pathify (ends-with-newline %)) paths)))))
 
 (defn -main [& argv]
   (let [cwd       (System/getProperty "user.dir")
         paths     (filter-blank argv)
-        fullpaths (map #(str cwd "/" %) paths)]
-    (print (format-paths fullpaths))
+        fullpaths (if (= 0 (count paths))
+                      (line-seq (java.io.BufferedReader. *in*))
+                      paths)
+        ]
+    (print (format-paths cwd fullpaths))
     (flush))) ; <-- ...uhm, why do I have to do this?
