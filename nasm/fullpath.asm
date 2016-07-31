b59fc695af43e2d854d93944719d01ba2e989350
Josh Cheek <josh.cheek@gmail.com>
2016-07-31 11:46:01 -0500

    Assembly: Update the hello world example to use an includes file
    Based on code from:
      http://www.freebsd.org/doc/en/books/developers-handbook/book.html#X86-SYSTEM-CALLS
    
    Specifically these sections:
      11.5.5. Using an Include File
      11.6.   Our First Program

--- nasm/fullpath.asm
+++ nasm/fullpath.asm
@@ -1,40 +1,16 @@
-section .text
-
-global main                   ; make the main function externally visible
-
-; a procedure wrapping the system call to write
-mywrite:
-    mov eax, 0x4              ; system call write code
-    int 0x80                  ; make system call
-    ret
-
-; a procedure wrapping the system call to exit
-myexit:
-    mov eax, 0x1              ; system call exit code
-    int 0x80                  ; make system call
-    ; no need to return
-
-main:
-; 1 print "hello, world"
-
-    ; 1a prepare arguments
-    push dword mylen           ; message length
-    push dword mymsg           ; message to write
-    push dword 1               ; file descriptor value
-    ; 1b make call
-    call mywrite
-    ; 1c clean up stack
-    add esp, 12
-
-; 2 exit the program
-
-    ; 2a prepare arguments
-    push dword 0              ; exit code
-    ; 2b make call
-    call myexit
-    ; 2c no need to clean up because no code here would executed...already exited!
+%include 'system.inc'
 
 section .data
+  hello db 'Hello, World!', 0Ah
+  hbytes equ $-hello
 
-  mymsg db "hello, world", 0xa  ; string with a carriage-return
-  mylen equ $-mymsg             ; string length in bytes
+section .text
+global main
+main:
+  push dword hbytes
+  push dword hello
+  push dword stdout
+  sys.write
+
+  push dword 0
+  sys.exit
