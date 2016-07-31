7ef20910192508ebdeea49e33f7733ff9f5ff31e
Josh Cheek <josh.cheek@gmail.com>
2016-07-30 23:31:04 -0500

    Clojure: Filters out blank paths

--- clojure/src/fullpath/core.clj
+++ clojure/src/fullpath/core.clj
@@ -1,6 +1,10 @@
 (ns fullpath.core)
 
+(defn filter-blank [args]
+  (filter #(not (clojure.string/blank? %)) args))
+
 (defn -main [& argv]
-  (let [cwd (System/getProperty "user.dir")
-        fullpaths (map #(str cwd "/" %) argv)]
+  (let [cwd       (System/getProperty "user.dir")
+        paths     (filter-blank argv)
+        fullpaths (map #(str cwd "/" %) paths)]
     (println fullpaths)))
