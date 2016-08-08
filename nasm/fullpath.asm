6a66f564867fbdbe3ad91ef6024e7c8e4e4954b1
Josh Cheek <josh.cheek@gmail.com>
2016-08-08 18:56:34 -0500

    Assembly: Prints "hello world" for every argument it received

--- nasm/fullpath.asm
+++ nasm/fullpath.asm
@@ -6,10 +6,24 @@ section .data
 
 section .text
 global main
-main:
-  call printit
-  sys.exit 0
 
+main:
+  pop ecx ; argc
+  pop eax ; filename
+  pop eax
+  add ecx, -1
+  .pop_arg
+    pop eax
+    sys.write hbytes, hello, stdout
+    ; call printit
+    add ecx, -1
+  jecxz .done
+  jmp .pop_arg
+  .done
+  sys.exit ecx
+
+; prints a null-terminated string pointed to by eax
 printit:
-  sys.write hbytes, hello, stdout
+  mov ebx, [eax]
+  ; sys.write 1
   ret
