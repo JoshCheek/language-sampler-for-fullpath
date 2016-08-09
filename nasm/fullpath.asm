db6d2bdc8433e62093dff46a0f11a42182d94c6e
Josh Cheek <josh.cheek@gmail.com>
2016-08-09 04:06:10 -0500

    Assembly: Prints the argumens

--- nasm/fullpath.asm
+++ nasm/fullpath.asm
@@ -1,8 +1,8 @@
 %include 'system.inc'
 
 section .data
-  hello   db  'Hello, World!', 0Ah
-  hbytes  equ $-hello
+  newline       db  0Ah
+  newline_size  equ $-newline
 
 section .text
 global main
@@ -12,12 +12,26 @@ main:
   pop eax ; filename
   dec ecx ; don't count the filename
   .pop_arg
-    pop eax
     call printit
+    pop eax
     dec ecx
     jnz .pop_arg
   sys.exit ecx
 
-; pops and prints a null-terminated string from the stack
+; prints a null-terminated string from the stack
 printit:
-  sys.write hbytes, hello, stdout
+  mov eax, [esp+4]
+  push ecx
+  mov ecx, 0
+  .count_chars
+    cmp [eax+ecx], byte 0
+    je .done_counting
+    inc ecx
+    jmp .count_chars
+  .done_counting
+  jecxz .done_printing
+    sys.write ecx, eax, stdout
+    sys.write newline_size, newline, stdout
+  .done_printing
+  pop ecx
+  ret
