2caa2e5773893b0c7afe914d85b36f3d1ff60656
Josh Cheek <josh.cheek@gmail.com>
2016-08-21 18:52:47 -0500

    Haxe: reads a line from stdin when no args

--- haxe/Fullpath.hx
+++ haxe/Fullpath.hx
@@ -7,12 +7,20 @@ class Fullpath {
       dir = dir.substr(0, dir.length-1);
     }
 
+    if(args.length == 0) {
+      args = readLines(Sys.stdin());
+    }
+
     if(args.length == 1) {
-      Sys.print(dir+args[0]);
+      Sys.stdout().writeString(dir+args[0]);
     } else {
       for(arg in args) {
-        Sys.println(dir+arg);
+        Sys.stdout().writeString(dir+arg+"\n");
       }
     }
+  }
+
+  static public function readLines(instream:haxe.io.Input) {
+    return [instream.readLine()];
   }
 }
