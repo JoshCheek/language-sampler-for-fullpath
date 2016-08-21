6bc30b9ef54c5cc0747aec14b0dc6053f26abb6f
Josh Cheek <josh.cheek@gmail.com>
2016-08-21 18:40:07 -0500

    Haxe: prints dir and files from args

--- haxe/Fullpath.hx
+++ haxe/Fullpath.hx
@@ -1,5 +1,11 @@
 class Fullpath {
   static public function main() {
-    Sys.println(Sys.args());
+    var dir = Sys.getCwd();
+    if(dir.charAt(dir.length-1) == "\n") {
+      dir = dir.substr(0, dir.length-1);
+    }
+    for(arg in Sys.args()) {
+      Sys.println(dir+arg);
+    }
   }
 }
