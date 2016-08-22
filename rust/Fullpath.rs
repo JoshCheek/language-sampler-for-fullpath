bdf35f2bed3c302b97948d14205a8c9711b6f57b
Josh Cheek <josh.cheek@gmail.com>
2016-08-22 02:07:06 -0500

    Rust: better failure handling

--- rust/Fullpath.rs
+++ rust/Fullpath.rs
@@ -1,12 +1,17 @@
 use std::env;
+use std::process;
 
 // This code is editable and runnable!
 fn main() {
     // let pwd:collections::string::String = "".to_string();
     let mut pwd = "".to_string();
     match env::current_dir() {
-        Ok(_pwd) => pwd = format!("{}", _pwd.display()),
-        Err(err) => println!("{}", err)
+        Ok(_pwd) =>
+            pwd = format!("{}", _pwd.display()),
+        Err(err) => {
+            println!("{}", err);
+            process::exit(1);
+        }
     }
     println!("{}", pwd);
     for argument in env::args() {
