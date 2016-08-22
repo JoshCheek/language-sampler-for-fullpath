d586f04baaeaa2b5a51e2521fb87d4c29e5ade2d
Josh Cheek <josh.cheek@gmail.com>
2016-08-22 02:24:54 -0500

    Rust: Refactor arg getting code into a function returning a vector

--- rust/Fullpath.rs
+++ rust/Fullpath.rs
@@ -4,9 +4,8 @@ use std::process;
 // This code is editable and runnable!
 fn main() {
     // let pwd:collections::string::String = "".to_string();
-    let pwd = get_pwd();
-    let mut args = env::args();
-    args.next(); // first arg is the program name
+    let pwd  = get_pwd();
+    let args = get_args();
     for argument in args {
         println!("{}/{}", pwd, argument);
     }
@@ -21,4 +20,10 @@ fn get_pwd() -> String {
             process::exit(1);
         }
     }
+}
+
+fn get_args() -> Vec<String> {
+    let mut argv = env::args();
+    argv.next(); // first arg is the program name
+    return argv.collect();
 }
