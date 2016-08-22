d8fb3815d7e707368848d16573eb13d4ac0ff3b0
Josh Cheek <josh.cheek@gmail.com>
2016-08-22 02:48:25 -0500

    Rust: Reads stdin (doesn't turn it into a list of paths yet, though)

--- rust/Fullpath.rs
+++ rust/Fullpath.rs
@@ -1,12 +1,24 @@
 use std::env;
 use std::process;
+use std::io::{self, Read};
 
 // This code is editable and runnable!
 fn main() {
     // let pwd:collections::string::String = "".to_string();
-    let pwd   = get_pwd();
-    let paths:Vec<String> =
+    let pwd = get_pwd();
+    let mut paths:Vec<String> =
         get_args().map(|path| format!("{}/{}", pwd, path)).collect();
+    if paths.len() == 0 {
+        let mut buffer = String::new();
+        match io::stdin().read_to_string(&mut buffer) {
+            Ok(_)    => paths = vec![buffer],
+            Err(err) => {
+                println!("{}", err);
+                process::exit(1);
+            }
+        }
+    }
+
 
     if paths.len() == 1 {
         print!("{}", paths[0]);
