c86050343530a972c38991b333373976f41ab1ca
Josh Cheek <josh.cheek@gmail.com>
2016-08-22 02:36:33 -0500

    Rust: Refactor formatting code to occur in one place.

--- rust/Fullpath.rs
+++ rust/Fullpath.rs
@@ -4,13 +4,15 @@ use std::process;
 // This code is editable and runnable!
 fn main() {
     // let pwd:collections::string::String = "".to_string();
-    let pwd  = get_pwd();
-    let args = get_args();
-    if args.len() == 1 {
-        print!("{}/{}", pwd, args[0]);
+    let pwd   = get_pwd();
+    let paths:Vec<String> =
+        get_args().map(|path| format!("{}/{}", pwd, path)).collect();
+
+    if paths.len() == 1 {
+        print!("{}", paths[0]);
     } else {
-        for argument in args {
-            println!("{}/{}", pwd, argument);
+        for path in paths {
+            println!("{}", path);
         }
     }
 }
@@ -26,8 +28,8 @@ fn get_pwd() -> String {
     }
 }
 
-fn get_args() -> Vec<String> {
+fn get_args() -> env::Args {
     let mut argv = env::args();
     argv.next(); // first arg is the program name
-    return argv.collect();
+    return argv;
 }
