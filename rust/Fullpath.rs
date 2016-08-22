c8a916e412db9b682733ca96d1f370007484ef08
Josh Cheek <josh.cheek@gmail.com>
2016-08-22 02:12:01 -0500

    Rust: Refactor PWD typechecking code into its own function

--- rust/Fullpath.rs
+++ rust/Fullpath.rs
@@ -4,18 +4,20 @@ use std::process;
 // This code is editable and runnable!
 fn main() {
     // let pwd:collections::string::String = "".to_string();
-    let pwd;
-    match env::current_dir() {
-        Ok(_pwd) =>
-            pwd = format!("{}", _pwd.display()),
-        Err(err) => {
-            println!("{}", err);
-            process::exit(1);
-        }
-    }
+    let pwd = get_pwd();
     println!("{}", pwd);
     for argument in env::args() {
         println!("{}", argument);
     }
 }
 
+fn get_pwd() -> String {
+    match env::current_dir() {
+        Ok(pwd) =>
+            return format!("{}", pwd.display()),
+        Err(err) => {
+            println!("{}", err);
+            process::exit(1);
+        }
+    }
+}
