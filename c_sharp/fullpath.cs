5bc00e895ecc2e250715e087afb4df2a231b7b4b
Josh Cheek <josh.cheek@gmail.com>
2016-09-20 21:28:55 -0700

    C Sharp: Gets command line args

--- c_sharp/fullpath.cs
+++ c_sharp/fullpath.cs
@@ -1,7 +1,11 @@
 using System;
 
 public class HelloWorld {
-    static public void Main () {
-        Console.WriteLine ("Hello Mono World");
+  static public void Main (String[] args) {
+    Console.WriteLine("Number of command line parameters = {0}",
+        args.Length);
+    for(int i = 0; i < args.Length; i++) {
+      Console.WriteLine("Arg[{0}] = [{1}]", i, args[i]);
     }
+  }
 }
