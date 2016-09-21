9baf99e78394f25282e14b2457e185f1ba4b76d1
Josh Cheek <josh.cheek@gmail.com>
2016-09-20 21:31:00 -0700

    C Sharp: Prints the path after the CWD

--- c_sharp/fullpath.cs
+++ c_sharp/fullpath.cs
@@ -1,11 +1,12 @@
 using System;
+using System.IO;
 
 public class HelloWorld {
   static public void Main (String[] args) {
-    Console.WriteLine("Number of command line parameters = {0}",
-        args.Length);
+    string path = Directory.GetCurrentDirectory();
+
     for(int i = 0; i < args.Length; i++) {
-      Console.WriteLine("Arg[{0}] = [{1}]", i, args[i]);
+      Console.WriteLine("{0}/{1}", path, args[i]);
     }
   }
 }
