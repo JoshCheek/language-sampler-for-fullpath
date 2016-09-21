e8a9327a801b694eab44d5793cf15ffb0dbe0ea3
Josh Cheek <josh.cheek@gmail.com>
2016-09-20 22:13:51 -0700

    C Sharp: Correctly handles newline, regardless of whether paths came from stdin or argv

--- c_sharp/fullpath.cs
+++ c_sharp/fullpath.cs
@@ -5,24 +5,36 @@ using System.Collections.Generic;
 public class HelloWorld {
   static public void Main (String[] args) {
     string cwd = Directory.GetCurrentDirectory();
+    List<String> paths = new List<String>();
 
-    if(args.Length == 1) {
-      Console.Write("{0}/{1}", cwd, args[0]);
-    } else if (args.Length > 0) {
-      for(int i = 0; i < args.Length; i++) {
-        Console.WriteLine("{0}/{1}", cwd, args[i]);
-      }
-    } else {
-      List<String> lines = new List<String>();
+    foreach(string arg in args) {
+      paths.Add(arg);
+    }
+
+    if(paths.Count == 0) {
       while(true) {
         string line = Console.ReadLine();
         if(line == null)
           break;
-        lines.Add(line);
-      }
-      foreach(string line in lines) {
-        Console.WriteLine("{0}/{1}", cwd, line);
+        paths.Add(line);
       }
     }
+
+    paths = Expand(cwd, paths);
+    foreach(string fullpath in paths) {
+      Console.Write(fullpath);
+    }
+  }
+
+  static public List<String> Expand(String cwd, List<String> paths) {
+    List<String> expanded = new List<String>();
+    if(paths.Count == 1) {
+      expanded.Add(cwd+"/"+paths[0]);
+    } else {
+      foreach(string path in paths) {
+        expanded.Add(cwd+"/"+path+"\n");
+      }
+    }
+    return expanded;
   }
 }
