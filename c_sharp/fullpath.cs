25c57e732606847afeadf85919e2bc5afc9c6443
Josh Cheek <josh.cheek@gmail.com>
2016-09-20 21:54:54 -0700

    C Sharp: Reads a line

--- c_sharp/fullpath.cs
+++ c_sharp/fullpath.cs
@@ -6,10 +6,20 @@ public class HelloWorld {
     string cwd = Directory.GetCurrentDirectory();
 
     if(args.Length == 1) {
-        Console.Write("{0}/{1}", cwd, args[0]);
-    } else {
+      Console.Write("{0}/{1}", cwd, args[0]);
+    } else if (args.Length > 0) {
       for(int i = 0; i < args.Length; i++) {
         Console.WriteLine("{0}/{1}", cwd, args[i]);
+      }
+    } else {
+      /* List<int> lines = new List<String>(); */
+      /* integers.Add(line); */
+      string line = Console.ReadLine();
+      Console.WriteLine("("+line+")");
+      if(line == null) {
+        Console.WriteLine("t("+line+")");
+      } else {
+        Console.WriteLine("f("+line+")");
       }
     }
   }
