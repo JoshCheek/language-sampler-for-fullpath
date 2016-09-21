e9993f81ebb48f2a5cfa84fdcb920d478cafe868
Josh Cheek <josh.cheek@gmail.com>
2016-09-20 21:39:59 -0700

    C Sharp: Omit newline when there is only one path

--- c_sharp/fullpath.cs
+++ c_sharp/fullpath.cs
@@ -3,10 +3,14 @@ using System.IO;
 
 public class HelloWorld {
   static public void Main (String[] args) {
-    string path = Directory.GetCurrentDirectory();
+    string cwd = Directory.GetCurrentDirectory();
 
-    for(int i = 0; i < args.Length; i++) {
-      Console.WriteLine("{0}/{1}", path, args[i]);
+    if(args.Length == 1) {
+        Console.Write("{0}/{1}", cwd, args[0]);
+    } else {
+      for(int i = 0; i < args.Length; i++) {
+        Console.WriteLine("{0}/{1}", cwd, args[i]);
+      }
     }
   }
 }
