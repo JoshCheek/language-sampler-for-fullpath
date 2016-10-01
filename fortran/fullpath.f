930232aa0a2f943f1c20fc19ec531d5944a9935b
Josh Cheek <josh.cheek@gmail.com>
2016-10-01 13:42:05 -0500

    Fortran: Print the dir with the path

--- fortran/fullpath.f
+++ fortran/fullpath.f
@@ -3,7 +3,7 @@
         INTEGER :: i, ipath, num_paths
         ! Apparently I can't jave a string of unknown length, so I'm
         ! just making it large enough to hold most things it could see
-        CHARACTER(len=2048) :: arg, path
+        CHARACTER(len=2048) :: arg, path, dir
 
         ! A dynamically sized array
         CHARACTER(len=2048), ALLOCATABLE, DIMENSION(:):: paths
@@ -29,11 +29,10 @@
         END DO
 
         ! Print the paths
-        DO ipath=0, num_paths
+        CALL getcwd(dir)
+        DO ipath=0, num_paths-1
           path = paths(ipath)
-          ! the (a) means its printing a string
-          WRITE (*,'(a)') TRIM(path)
-          WRITE (*,*) LEN_TRIM(path)
+          WRITE (*,'(aaa)') TRIM(dir), "/", TRIM(path)
         END DO
 
         deallocate(paths)
