e991eb8efd042cdeef7cd864a71355d7cd8bde66
Josh Cheek <josh.cheek@gmail.com>
2016-10-01 13:33:17 -0500

    Fortran: Copy the paths into a list instead of printing them directly

--- fortran/fullpath.f
+++ fortran/fullpath.f
@@ -1,30 +1,40 @@
       PROGRAM test_get_command_argument
         IMPLICIT NONE
-        INTEGER :: i, num_paths
+        INTEGER :: i, ipath, num_paths
         ! Apparently I can't jave a string of unknown length, so I'm
         ! just making it large enough to hold most things it could see
-        CHARACTER(len=2048) :: arg
+        CHARACTER(len=2048) :: arg, path
 
         ! A dynamically sized array
-        INTEGER, ALLOCATABLE, DIMENSION(:):: paths
+        CHARACTER(len=2048), ALLOCATABLE, DIMENSION(:):: paths
 
-        ! allocate(paths(num_paths))
+        ! Categorize arguments (path, flag, etc)
+        num_paths = 0
         DO i = 1, iargc()
           CALL getarg(i, arg)
           IF (LEN_TRIM(arg) /= 0) THEN
-            ! the (a) means its printing a string
-            WRITE (*,'(a)') TRIM(arg)
-            WRITE (*,*) LEN_TRIM(arg)
+            num_paths = num_paths + 1
           END IF
         END DO
 
-        ! deallocate(paths)
+        ! Get the paths
+        allocate(paths(num_paths))
+        ipath = 0
+        DO i = 1, iargc()
+          CALL getarg(i, arg)
+          IF (LEN_TRIM(arg) /= 0) THEN
+            paths(ipath) = arg
+            ipath = ipath + 1
+          END IF
+        END DO
+
+        ! Print the paths
+        DO ipath=0, num_paths
+          path = paths(ipath)
+          ! the (a) means its printing a string
+          WRITE (*,'(a)') TRIM(path)
+          WRITE (*,*) LEN_TRIM(path)
+        END DO
+
+        deallocate(paths)
       END PROGRAM
-
-      ! do i=1,elements
-      !      read *,vector(i)
-      ! end do
-
-      ! do i=1,elements
-      !      print *,vector(i)
-      ! end do
