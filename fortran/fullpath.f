af60c139d554c0d6d6b7eb2f8d36754012a53b8b
Josh Cheek <josh.cheek@gmail.com>
2016-10-01 13:43:12 -0500

    Fortran: Initialize some arguments when declaring them

--- fortran/fullpath.f
+++ fortran/fullpath.f
@@ -1,6 +1,6 @@
       PROGRAM test_get_command_argument
         IMPLICIT NONE
-        INTEGER :: i, ipath, num_paths
+        INTEGER :: i, ipath=0, num_paths=0
         ! Apparently I can't jave a string of unknown length, so I'm
         ! just making it large enough to hold most things it could see
         CHARACTER(len=2048) :: arg, path, dir
@@ -9,7 +9,6 @@
         CHARACTER(len=2048), ALLOCATABLE, DIMENSION(:):: paths
 
         ! Categorize arguments (path, flag, etc)
-        num_paths = 0
         DO i = 1, iargc()
           CALL getarg(i, arg)
           IF (LEN_TRIM(arg) /= 0) THEN
@@ -19,7 +18,6 @@
 
         ! Get the paths
         allocate(paths(num_paths))
-        ipath = 0
         DO i = 1, iargc()
           CALL getarg(i, arg)
           IF (LEN_TRIM(arg) /= 0) THEN
