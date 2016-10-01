4b62575ad9d211faf5aa8815696f2531ca931983
Josh Cheek <josh.cheek@gmail.com>
2016-10-01 13:27:19 -0500

    Fortran: Don't print empty paths

--- fortran/fullpath.f
+++ fortran/fullpath.f
@@ -1,12 +1,30 @@
       PROGRAM test_get_command_argument
-        INTEGER :: i
+        IMPLICIT NONE
+        INTEGER :: i, num_paths
         ! Apparently I can't jave a string of unknown length, so I'm
         ! just making it large enough to hold most things it could see
         CHARACTER(len=2048) :: arg
 
+        ! A dynamically sized array
+        INTEGER, ALLOCATABLE, DIMENSION(:):: paths
+
+        ! allocate(paths(num_paths))
         DO i = 1, iargc()
           CALL getarg(i, arg)
-          ! the (a) means its printing a string
-          WRITE (*,'(a)') TRIM(arg)
+          IF (LEN_TRIM(arg) /= 0) THEN
+            ! the (a) means its printing a string
+            WRITE (*,'(a)') TRIM(arg)
+            WRITE (*,*) LEN_TRIM(arg)
+          END IF
         END DO
+
+        ! deallocate(paths)
       END PROGRAM
+
+      ! do i=1,elements
+      !      read *,vector(i)
+      ! end do
+
+      ! do i=1,elements
+      !      print *,vector(i)
+      ! end do
