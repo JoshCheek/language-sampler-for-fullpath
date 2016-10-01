e08030ebe8bb495ae19fffe4631fa654adec96c7
Josh Cheek <josh.cheek@gmail.com>
2016-10-01 16:52:07 -0500

    Fortran: Read paths from stdin when they aren't present in argv

--- fortran/fullpath.f90
+++ fortran/fullpath.f90
@@ -1,4 +1,4 @@
-PROGRAM test_get_command_argument
+PROGRAM Fullpath
   IMPLICIT NONE
   INTEGER, PARAMETER :: strlen=2048
   INTEGER :: i, j, status=0, ipath=1, num_paths=0
@@ -8,9 +8,9 @@ PROGRAM test_get_command_argument
   CHARACTER(len=strlen) :: arg, path, dir
 
   ! A dynamically sized array
-  CHARACTER(len=strlen), ALLOCATABLE, DIMENSION(:):: paths, tmp
+  CHARACTER(len=strlen), DIMENSION(:), POINTER :: paths, tmp
 
-  ! Analyze argv
+  ! Analyze ARGV
   DO i = 1, iargc()
     CALL getarg(i, arg)
     IF (LEN_TRIM(arg) /= 0) THEN
@@ -32,13 +32,19 @@ PROGRAM test_get_command_argument
     END DO
   ELSE
     ! Read paths from stdin
-    ipath = 1
     num_paths = 0
+    allocate(paths(0))
     DO WHILE (.TRUE.)
+      ! Reset the path
+      DO i=1, strlen
+        path(i:i) = achar(0)
+      END DO
+
       ! Read one path
       i=1
       DO WHILE(.TRUE.)
         call fget(path(i:i), status)
+        ! break if we hit the end of the stream
         IF (status /= 0) THEN
           EXIT
         END IF
@@ -52,31 +58,40 @@ PROGRAM test_get_command_argument
 
       ! If we found a path
       IF (i /= 1) THEN
-        ! Increase counters
+        ! allocate room for one the existing and new path
+        allocate(tmp(num_paths+1))
+
+        ! copy the paths over
+        DO ipath=1, num_paths
+          tmp(ipath) = paths(ipath)
+        END DO
+
+        ! add the path we just read in
+        tmp(num_paths+1) = path
+
+        ! deallocate the old paths
+        deallocate(paths)
+
+        ! set them to the correct variable
+        paths => tmp
+        NULLIFY(tmp)
+
+        ! Increase counter
         num_paths = num_paths + 1
-        ipath = ipath + 1
-        ! tmp = allocate(num_paths)                          # allocate room for one num paths + 1
-        ! paths.each_with_index { |path, i| tmp[i] = path }  # copy the paths over
-        ! paths[ipath] = path                                # add the path we just read in
-        ! deallocate(paths)                                  # deallocate the old paths
-        ! paths = tmp                                        # set them to the correct variable
       END IF
 
-      ! No more paths
+      ! No more paths, we're done
       IF (status /= 0) THEN
         EXIT
       END IF
     END DO
-
-    num_paths = 1
-    allocate(paths(num_paths))
   END IF
+
 
   ! Print the paths
   CALL getcwd(dir)
-
   IF (num_paths == 1) THEN
-    path = paths(0)
+    path = paths(1)
     WRITE(*, '(3a)',advance="no") TRIM(dir), "/", TRIM(path)
   ELSE
     DO ipath=1, num_paths
