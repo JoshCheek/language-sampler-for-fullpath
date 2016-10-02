85f0b3fdfefde78d7a3e9e89bcf55744ccfec67f
Josh Cheek <josh.cheek@gmail.com>
2016-10-02 02:40:13 -0500

    Scheme: Checks for help and copy

--- scheme/fullpath.scm
+++ scheme/fullpath.scm
@@ -2,15 +2,41 @@
 
 (import posix)
 (use posix)
+(use srfi-1)
 
-(length (command-line-arguments))
-(if #t 1 2) ; 1
-(if #f 1 2) ; 2
 
-(let ((dir  (current-directory))
-      (argv (command-line-arguments)))
-  (if (= 1 (length argv))
-    (display (string-append dir "/" (car argv)))
-    (for-each
-      (lambda (arg) (print dir "/" arg))
-      argv)))
+; (print (read-line))
+(define (any? fn lst)
+  (if (null? lst)
+    #f
+    (or (fn (car lst))
+        (any? fn (cdr lst)))))
+
+(define (help-arg? arg)
+  (or (string=? "-h"     arg)
+      (string=? "--help" arg)))
+
+(define (copy-output-arg? arg)
+  (or (string=? "-c"      arg)
+      (string=? "--copy" arg)))
+
+(define (show-help? argv)
+  (any? help-arg? argv))
+
+(define (copy-output? argv)
+  (any? copy-output-arg? argv))
+
+(define (select-paths potentials)
+  (filter (lambda (potential)
+            (not (or (help-arg? potential)
+                     (copy-output-arg? potential))))
+          potentials))
+
+(let* ((dir         (current-directory))
+       (argv        (command-line-arguments))
+       (show-help   (show-help? argv))
+       (copy-output (copy-output? argv))
+       (paths       (select-paths argv)))
+    (if (= 1 (length paths))
+      (display (string-append dir "/" (car paths)))
+      (for-each (lambda (path) (print dir "/" path)) paths)))
