649d0684ad7d125c2838484f95773d138208fcd0
Josh Cheek <josh.cheek@gmail.com>
2016-10-01 22:53:50 -0500

    Scheme: Refactor multiple invocations of command-line-arguments to a variable

--- scheme/fullpath.scm
+++ scheme/fullpath.scm
@@ -7,9 +7,10 @@
 (if #t 1 2) ; 1
 (if #f 1 2) ; 2
 
-(let ((dir (current-directory)))
-  (if (= 1 (length (command-line-arguments)))
-    (display (string-append dir "/" (car (command-line-arguments))))
+(let ((dir  (current-directory))
+      (argv (command-line-arguments)))
+  (if (= 1 (length argv))
+    (display (string-append dir "/" (car argv)))
     (for-each
       (lambda (arg) (print dir "/" arg))
-      (command-line-arguments))))
+      argv)))
