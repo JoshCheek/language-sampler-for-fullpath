88c3216b13a4017fcbc3a1e4f1355671162d6333
Josh Cheek <josh.cheek@gmail.com>
2016-10-05 19:22:48 -0500

    Swift: Print the help screen

--- swift/Fullpath.swift
+++ swift/Fullpath.swift
@@ -1,9 +1,22 @@
 import Foundation
+import Darwin
 
 // First dir is the program name
 let args = ProcessInfo.processInfo.arguments.dropFirst()
 let dir  = FileManager.default.currentDirectoryPath
 var paths: ArraySlice<String> = []
+
+if args.contains("-h") || args.contains("--help") {
+  print("usage: fullpath *[relative-paths] [-c]")
+  print("")
+  print("  Prints the fullpath of the paths")
+  print("  If no paths are given as args, it will read them from stdin")
+  print("")
+  print("  If there is only one path, the trailing newline is omitted")
+  print("")
+  print("  The -c flag will copy the results into your pasteboard")
+  exit(0)
+}
 
 if args.count == 0 {
   while let line = readLine() {
