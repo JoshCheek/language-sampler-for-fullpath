caa5067f8c8464b4f875a78d3b5ecacde316d235
Josh Cheek <josh.cheek@gmail.com>
2016-10-05 19:20:04 -0500

    Swift: Read from stdin if there are no args

--- swift/Fullpath.swift
+++ swift/Fullpath.swift
@@ -3,13 +3,24 @@ import Foundation
 // First dir is the program name
 let args = ProcessInfo.processInfo.arguments.dropFirst()
 let dir  = FileManager.default.currentDirectoryPath
+var paths: ArraySlice<String> = []
 
-if args.count == 1 {
-  if let arg = args.first {
-    print("\(dir)/\(arg)", terminator: "")
+if args.count == 0 {
+  while let line = readLine() {
+    paths.append(line)
   }
 } else {
   for arg in args {
-    print("\(dir)/\(arg)")
+    paths.append(arg)
+  }
+}
+
+if paths.count == 1 {
+  if let path = paths.first {
+    print("\(dir)/\(path)", terminator: "")
+  }
+} else {
+  for path in paths {
+    print("\(dir)/\(path)")
   }
 }
